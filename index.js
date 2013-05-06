cdb53710623c5a561e651e12717d84f85ef26e04
TJ Holowaychuk <tj@vision-media.ca>
2013-05-05 20:30:43 -0700

    add implementation

--- index.js
+++ index.js
@@ -3,15 +3,40 @@
  * Module dependencies.
  */
 
-var Rect = require('rect');
 var inherit = require('inherit');
+var Rect = require('rect');
+var css = require('css');
 
 module.exports = Selection;
 
 function Selection(x, y, w, h) {
-  Rect.apply(this, arguments);
   this.el = document.createElement('div');
   this.el.className = 'selection-rect';
+  Rect.apply(this, arguments);
 }
 
 inherit(Selection, Rect);
+
+Selection.prototype.moveTo = function(x, y){
+  Rect.prototype.moveTo.apply(this, arguments);
+  css(this.el, {
+    top: y,
+    left: x
+  });
+};
+
+Selection.prototype.size = function(w, h){
+  Rect.prototype.size.apply(this, arguments);
+  css(this.el, {
+    width: this.w,
+    height: this.h
+  });
+};
+
+Selection.prototype.to = function(w, h){
+  Rect.prototype.to.apply(this, arguments);
+  css(this.el, {
+    width: this.w,
+    height: this.h
+  });
+};
